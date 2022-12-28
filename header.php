1e949aff25b779a24a6ca463a335a89027b40f87
sagomase <sagomase11@gmail.com>
2022-12-28 16:59:52 +0530

    changes done

--- header.php
+++ header.php
@@ -1,113 +1,113 @@
     <button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fa fa-chevron-up"></i></button>
-      <!--header starts-->
-      <section>
-      <div class="header homeHeader">
-         <div class="row toprow">
-            <div class="logoBox"></div>
-            <div class="menuTopBox">
-               <div class="l_margin-for-nav-col">
-                  <nav class="navbar navbar-expand-lg navbar-light special-color-light  text-right float-right">
-                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent2" aria-controls="navbarSupportedContent2" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
-                     <div class="collapse navbar-collapse" id="navbarSupportedContent2">
-                        <ul class="navbar-nav mr-auto">
-                           <li class="nav-item"><a class="nav-link" href=""><span>Blog</span></a></li>
-                           <li class="nav-item"><a class="nav-link" href="" ><span>Legal Health Checkup</span></a></li>
-                           <li class="nav-item"><a class="nav-link" href="" ><span>Digital Library</span></a> </li>
-                           <li class="nav-item l-r-margin-for-nav-item formnav">
-                              <form class="form-inline my-2 my-lg-0" class="searchform" method="get" name="searchform"  action="https://www.google.com/search">
-                                 <input name="sitesearch" type="hidden" value="example.com">
-                                 <input autocomplete="on" class="form-control search" name="q" placeholder="Site Search" required="required"  type="text">
-                                 <button class="button" type="submit"><i class="fa fa-search"></i></button>
-                              </form>
-                           </li>
-                        </ul>
-                     </div>
-                  </nav>
-               </div>
-            </div>
-         </div>
-         <div class="row bottomrow" style="margin-top: -5px;">
-            <div class="logoBox"><a href="https://razorcube.com/"><img src="images/RazorCube.png" class="logo logoHome" alt="RazorCube" title="RazorCube"></a></div>
-            <div class="menuTopBox">
-               <div class="l_margin-for-nav-col">
-                  <!-- Navbar -->
-                  <nav class="navbar navbar-expand-lg navbar-light special-color-light  text-right float-right">
-                     <!-- Navbar brand --> 
-                     <!-- Collapse button -->
-                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent2" aria-controls="navbarSupportedContent2" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
-                     <!-- Collapsible content -->
-                     <div class="collapse navbar-collapse" id="navbarSupportedContent2">
-                        <!-- Links -->
-                        <ul class="navbar-nav mr-auto">
-                           <li class="nav-item dropdown mega-dropdown" >
-                              <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink3" data-toggle="collapse" href="#s" aria-haspopup="true" aria-expanded="false">Products</a>
-                              <div class="collapse dropdown-menu mega-menu v-2 z-depth-1 special-color py-5 px-3" aria-labelledby="navbarDropdownMenuLink3">
-                                 <div class="row" id="s">
-                                    <div class="col">
-                                       <ul class="subMenuList">
-                                          <li> <a class="menu-item pl-0" href="">RazorCube Enterprise Legal Management</a></li>
-                                          <li> <a class="menu-item pl-0" href="">Razor365 Legal Contract Management</a></li>
-                                          <li> <a class="menu-item pl-0" href="">RazorCube Matter & Case Management</a></li>
-                                          <li> <a class="menu-item pl-0" href="">RazorCube Legal Spend & E-billing</a></li>
-                                          <li> <a class="menu-item pl-0" href="">RazorCube Risk Management</a></li>
-                                          <li> <a class="menu-item pl-0" href="">RazorCube Corporate Compliance Management Platform</a></li>
-                                          <li> <a class="menu-item pl-0" href="">RazorCube Legal Document Management</a></li>
-                                          <li> <a class="menu-item pl-0" href="">RazorCube IP Portfolio Management</a> </li>
-                                          <li> <a class="menu-item pl-0" href="">RazorCube Insider Trading</a> </li>
-                                          <li> <a class="menu-item pl-0" href="">RazorLex Law Firm Management</a> </li>
-                                       </ul>
-                                    </div>
-                                 </div>
-                              </div>
-                           </li>
-                           <!-- Company -->
-                           <li class="nav-item dropdown ">
-                              <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink4"  data-toggle="collapse" href="#s" aria-haspopup="true" aria-expanded="false">Company</a>
-                              <div class="collapse dropdown-menu v-2 z-depth-1 special-color py-5 px-3" aria-labelledby="navbarDropdownMenuLink4">
-                                 <div class="row" id="s">
-                                    <div class="col">
-                                       <ul class="subMenuList">
-                                          <li> <a class="menu-item pl-0" href="">Who We Are1 </a> </li>
-                                          <li> <a class="menu-item pl-0" href="">News</a> </li>
-                                          <li> <a class="menu-item pl-0" href="">Events </a> </li>
-                                          <li> <a class="menu-item pl-0" href="">Partners </a> </li>
-                                          <li> <a class="menu-item pl-0" href="">CSR </a> </li>
-                                          <li> <a class="menu-item pl-0" href="">Contact us </a> </li>
-                                       </ul>
-                                    </div>
-                                 </div>
-                              </div>
-                           </li>
-                           <!-- Executive Resources -->
-                           <li class="nav-item dropdown">
-                              <a class="nav-link dropdown-toggle " id="navbarDropdownMenuLink4"  data-toggle="collapse" href="#s" aria-haspopup="true" aria-expanded="false">Resources</a>
-                              <div class="collapse dropdown-menu mega-menu1 v-2 z-depth-1 special-color py-5 px-3" aria-labelledby="navbarDropdownMenuLink4">
-                                 <div class="row" id="s">
-                                    <div class="col">
-                                       <ul class="subMenuList">
-                                          <li><a class="menu-item pl-0" href="" > Digital Library </a> </li>
-                                          <li><a class="menu-item pl-0" href=""> Blog </a> </li>
-                                          <li><a class="menu-item pl-0" href=""> Insights for General Counsel </a> </li>
-                                          <li><a class="menu-item pl-0" href="" > Health Checkup </a> </li>
-                                       </ul>
-                                    </div>
-                                 </div>
-                              </div>
-                           </li>                          
-                       
-                           <li class="nav-item l-r-margin-for-nav-item"> <a href="" class="nav-link style-for-nav-link active font-24">
-                              <button type="button" class="btn btn-primary style-for-elm-btn">Request Demo</button>
-                              </a>
-                           </li>
-                        </ul>
-                        <!-- Links --> 
-                     </div>
-                     <!-- Collapsible content --> 
-                  </nav>
-                  <!-- Navbar --> 
-               </div>
-            </div>
-         </div>
-      </div>
-      </section>
-      <!--header ends-->
+    <!--header starts-->
+    <section>
+       <div class="header homeHeader">
+          <div class="row toprow">
+             <div class="logoBox"></div>
+             <div class="menuTopBox">
+                <div class="l_margin-for-nav-col">
+                   <nav class="navbar navbar-expand-lg navbar-light special-color-light  text-right float-right">
+                      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent2" aria-controls="navbarSupportedContent2" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+                      <div class="collapse navbar-collapse" id="navbarSupportedContent2">
+                         <ul class="navbar-nav mr-auto">
+                            <li class="nav-item"><a class="nav-link" href=""><span>Blog</span></a></li>
+                            <li class="nav-item"><a class="nav-link" href=""><span>Legal Health Checkup</span></a></li>
+                            <li class="nav-item"><a class="nav-link" href=""><span>Digital Library</span></a> </li>
+                            <li class="nav-item l-r-margin-for-nav-item formnav">
+                               <form class="form-inline my-2 my-lg-0" class="searchform" method="get" name="searchform" action="https://www.google.com/search">
+                                  <input name="sitesearch" type="hidden" value="example.com">
+                                  <input autocomplete="on" class="form-control search" name="q" placeholder="Site Search" required="required" type="text">
+                                  <button class="button" type="submit"><i class="fa fa-search"></i></button>
+                               </form>
+                            </li>
+                         </ul>
+                      </div>
+                   </nav>
+                </div>
+             </div>
+          </div>
+          <div class="row bottomrow" style="margin-top: -5px;">
+             <div class="logoBox"><a href="https://razorcube.com/"><img src="images/RazorCube.png" class="logo logoHome" alt="RazorCube" title="RazorCube"></a></div>
+             <div class="menuTopBox">
+                <div class="l_margin-for-nav-col">
+                   <!-- Navbar -->
+                   <nav class="navbar navbar-expand-lg navbar-light special-color-light  text-right float-right">
+                      <!-- Navbar brand -->
+                      <!-- Collapse button -->
+                      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent2" aria-controls="navbarSupportedContent2" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+                      <!-- Collapsible content -->
+                      <div class="collapse navbar-collapse" id="navbarSupportedContent2">
+                         <!-- Links -->
+                         <ul class="navbar-nav mr-auto">
+                            <li class="nav-item dropdown mega-dropdown">
+                               <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink3" data-toggle="collapse" href="#s" aria-haspopup="true" aria-expanded="false">Products</a>
+                               <div class="collapse dropdown-menu mega-menu v-2 z-depth-1 special-color py-5 px-3" aria-labelledby="navbarDropdownMenuLink3">
+                                  <div class="row" id="s">
+                                     <div class="col">
+                                        <ul class="subMenuList">
+                                           <li> <a class="menu-item pl-0" href="">RazorCube Enterprise Legal Management</a></li>
+                                           <li> <a class="menu-item pl-0" href="">Razor365 Legal Contract Management</a></li>
+                                           <li> <a class="menu-item pl-0" href="">RazorCube Matter & Case Management</a></li>
+                                           <li> <a class="menu-item pl-0" href="">RazorCube Legal Spend & E-billing</a></li>
+                                           <li> <a class="menu-item pl-0" href="">RazorCube Risk Management</a></li>
+                                           <li> <a class="menu-item pl-0" href="">RazorCube Corporate Compliance Management Platform</a></li>
+                                           <li> <a class="menu-item pl-0" href="">RazorCube Legal Document Management</a></li>
+                                           <li> <a class="menu-item pl-0" href="">RazorCube IP Portfolio Management</a> </li>
+                                           <li> <a class="menu-item pl-0" href="">RazorCube Insider Trading</a> </li>
+                                           <li> <a class="menu-item pl-0" href="">RazorLex Law Firm Management</a> </li>
+                                        </ul>
+                                     </div>
+                                  </div>
+                               </div>
+                            </li>
+                            <!-- Company -->
+                            <li class="nav-item dropdown ">
+                               <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink4" data-toggle="collapse" href="#s" aria-haspopup="true" aria-expanded="false">Company</a>
+                               <div class="collapse dropdown-menu v-2 z-depth-1 special-color py-5 px-3" aria-labelledby="navbarDropdownMenuLink4">
+                                  <div class="row" id="s">
+                                     <div class="col">
+                                        <ul class="subMenuList">
+                                           <li> <a class="menu-item pl-0" href="">Who We Are1 </a> </li>
+                                           <li> <a class="menu-item pl-0" href="">News</a> </li>
+                                           <li> <a class="menu-item pl-0" href="">Events </a> </li>
+                                           <li> <a class="menu-item pl-0" href="">Partners </a> </li>
+                                           <li> <a class="menu-item pl-0" href="">CSR </a> </li>
+                                           <li> <a class="menu-item pl-0" href="">Contact us </a> </li>
+                                        </ul>
+                                     </div>
+                                  </div>
+                               </div>
+                            </li>
+                            <!-- Executive Resources -->
+                            <li class="nav-item dropdown">
+                               <a class="nav-link dropdown-toggle " id="navbarDropdownMenuLink4" data-toggle="collapse" href="#s" aria-haspopup="true" aria-expanded="false">Resources</a>
+                               <div class="collapse dropdown-menu mega-menu1 v-2 z-depth-1 special-color py-5 px-3" aria-labelledby="navbarDropdownMenuLink4">
+                                  <div class="row" id="s">
+                                     <div class="col">
+                                        <ul class="subMenuList">
+                                           <li><a class="menu-item pl-0" href=""> Digital Library </a> </li>
+                                           <li><a class="menu-item pl-0" href=""> Blog </a> </li>
+                                           <li><a class="menu-item pl-0" href=""> Insights for General Counsel </a> </li>
+                                           <li><a class="menu-item pl-0" href=""> Health Checkup </a> </li>
+                                        </ul>
+                                     </div>
+                                  </div>
+                               </div>
+                            </li>
+
+                            <li class="nav-item l-r-margin-for-nav-item"> <a href="" class="nav-link style-for-nav-link active font-24">
+                                  <button type="button" class="btn btn-primary style-for-elm-btn">Request Demo</button>
+                               </a>
+                            </li>
+                         </ul>
+                         <!-- Links -->
+                      </div>
+                      <!-- Collapsible content -->
+                   </nav>
+                   <!-- Navbar -->
+                </div>
+             </div>
+          </div>
+       </div>
+    </section>
+    <!--header ends-->
